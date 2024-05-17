4752461d62b9594945f47922703132ea38792dd1
bolivarmenezes <bolivar@redes.ufsm.br>
2024-05-17 17:35:18 -0300

    resolvendo bug de conexões abertas para os verbos do HTTP diferentes de GET e POST

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,32 +3,45 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationsOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra/migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pg_migrations",
-  };
-
-  if (request.method === "GET") {
-    const pengingMigrations = await migrationRunner(defaultMigrationsOptions);
-    await dbClient.end();
-    return response.status(200).json(pengingMigrations);
-  }
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false,
+  if (request.method != "GET" && request.method != "POST") {
+    console.log(`Recebeu um ${request.method}`);
+    return response.status(405).json({
+      error: `Method ${request.method} not allowed`,
     });
-    await dbClient.end();
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
-    }
-    return response.status(200).json(migratedMigrations);
   }
-  response.status(405);
+
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationsOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra/migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pg_migrations",
+    };
+
+    if (request.method === "GET") {
+      const pengingMigrations = await migrationRunner(defaultMigrationsOptions);
+      return response.status(200).json(pengingMigrations);
+    }
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: false,
+      });
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+      return response.status(200).json(migratedMigrations);
+    }
+    response.status(405);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,5 @@
-const { version } = require("react");
+import database from "infra/database.js";
+import axios from "axios";
 
 test("GET to /api/v1/status return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
@@ -11,8 +12,28 @@ test("GET to /api/v1/status return 200", async () => {
   expect(responseBody.dependencies.database.version).toEqual("16.3");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+});
 
-  /*const version_db = responseBody.version_db;
+test("Verifica se fechou as conexões com o banco de dados", async () => {
+  const executaHTTPUpdate = await fetch(
+    "http://localhost:3000/api/v1/migrations",
+    {
+      method: "DELETE",
+    },
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname= $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+  console.log(databaseOpenedConnectionsValue);
+  expect(databaseOpenedConnectionsValue).toBe(1);
+});
+
+/*const version_db = responseBody.version_db;
   expect(version_db).toBeDefined();
   expect(version_db).toContain("Postgre");
 
@@ -21,4 +42,3 @@ test("GET to /api/v1/status return 200", async () => {
 
   const used_connections_db = responseBody.used_connections_db;
   expect(used_connections_db).toBeDefined();*/
-});
